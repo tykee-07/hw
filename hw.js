efd2790aa1f629f6dee15d201dad524014cf257e
Tuigun <akysbekovtuigun@gmail.com>
2025-01-30 09:46:55 +0600

    tykee

--- hw.js
+++ hw.js
@@ -1,3 +1,7 @@
 
 let about=document.querySelector(".about")
 about.innerHTML="hello git"
+let dcf=document.querySelector(".dcf")
+dcf.style.border="1px solid black"
+dcf.style.margin="0 auto"
+dcf.style.width="400px"
